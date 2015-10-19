56f0cf0feee75980075bc29a49eee2499ef42683
shibutani <karin.shibutani.2t@stu.hosei.ac.jp>
2015-10-19 15:24:52 +0900

    コメント追加

--- app/src/main/java/com/example/kato/helloworld/MainActivity.java
+++ app/src/main/java/com/example/kato/helloworld/MainActivity.java
@@ -5,12 +5,18 @@ import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,10 +24,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.provider.MediaStore;
+import android.text.SpannableStringBuilder;
+import android.text.TextWatcher;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity{
+// implements LocationListener
     private static final int RESULT_PICK_IMAGEFILE = 1001;
     private ImageView imageView;
     private Button button;
@@ -34,9 +44,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView)findViewById(R.id.image_view);
+        imageView = (ImageView) findViewById(R.id.image_view);
 
-        button = (Button)findViewById(R.id.button);
+        button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,13 +64,108 @@ public class MainActivity extends Activity {
             }
         });
 
+        /*setContentView(R.layout.activity_main);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setHeight(50);
+
+        //SpannableStringBuilder sb = (SpannableStringBuilder)edit.getText();
+        String str = editText.getText().toString();
+
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(str);
+        */
+
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText(s.toString());
+
+            }
+        });
+    }
+        /*super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // LocationManagerを取得
+        LocationManager mLocationManager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Criteriaオブジェクトを生成
+        Criteria criteria = new Criteria();
+
+        // Accuracyを指定(低精度)
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+
+        // PowerRequirementを指定(低消費電力)
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+
+        // ロケーションプロバイダの取得
+        String provider = mLocationManager.getBestProvider(criteria, true);
+
+        // 取得したロケーションプロバイダを表示
+        //TextView tv_provider = (TextView) findViewById(R.id.Provider);
+        //tv_provider.setText("Provider: "+provider);
+
+        // LocationListenerを登録
+        mLocationManager.requestLocationUpdates(provider, 0, 0, this);
+
     }
 
-    // アプリからギャラリーにアクセスして、画像と画像情報を取得 からの戻り
-    protected void onActivityResult( int requestCode, int resultCode, Intent intent) {
+    @Override
+    public void onLocationChanged(Location location) {
+        // 緯度の表示
+        TextView tv_lat = (TextView) findViewById(R.id.Latitude);
+        tv_lat.setText("緯度:"+location.getLatitude());
+
+        // 経度の表示
+        TextView tv_lng = (TextView) findViewById(R.id.Longitude);
+        tv_lng.setText("経度:"+location.getLongitude());
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+*/
+
+
+        // アプリからギャラリーにアクセスして、画像と画像情報を取得 からの戻り
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
 
-        if(requestCode == RESULT_PICK_IMAGEFILE && resultCode == RESULT_OK && null != intent) {
+
+        if (requestCode == RESULT_PICK_IMAGEFILE && resultCode == RESULT_OK && null != intent) {
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inJustDecodeBounds = false;
             Bitmap bmp = null;
@@ -77,7 +182,7 @@ public class MainActivity extends Activity {
                 e.printStackTrace();
             }
 
-            if(bmp != null){
+            if (bmp != null) {
                 imageView.setImageBitmap(bmp);
             }
 
@@ -87,3 +192,5 @@ public class MainActivity extends Activity {
     private void getImageDataPath(Uri selectedImageURI) {
     }
 }
+
+
